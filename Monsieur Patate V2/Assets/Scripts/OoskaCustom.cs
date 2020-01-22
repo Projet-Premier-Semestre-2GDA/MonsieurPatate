55451bb5def12026256327a21dfc01d972d346e7
Arcanis <bastienbarth@gmail.com>
2020-01-22 17:16:47 +0100

    Plusieurs modifications (FMOD, modèles, matériaux)
    -matériaux pâte à modeler testés
    -fmod ajouté
    - modèles de l'avatar changé

--- Monsieur Patate V2/Assets/Scripts/OoskaCustom.cs
+++ Monsieur Patate V2/Assets/Scripts/OoskaCustom.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public static class OoskaCustom {
     static Dictionary<string, bool> AxisDownDict = new Dictionary<string, bool>();
-
     public static bool GetAxisDown(string axisName) {
         float absoluteAxisValue = Mathf.Abs(Input.GetAxisRaw(axisName));
         bool isAxisDown = absoluteAxisValue > 0.1;
@@ -21,5 +20,22 @@ public static class OoskaCustom {
         
         AxisDownDict[axisName] = isAxisDown;
         return isAxisDown;
+    }
+
+    public static bool GetAxisUp(string axisName) {
+        float absoluteAxisValue = Mathf.Abs(Input.GetAxisRaw(axisName));
+        bool isAxisUp = absoluteAxisValue < 0.1;
+        
+        //Ajouter l'axe au dictionaire
+        if (!AxisDownDict.ContainsKey(axisName)) {
+            AxisDownDict.Add(axisName, true);
+        }
+
+        if (isAxisUp && !AxisDownDict[axisName]) {
+            return false;
+        }
+
+        AxisDownDict[axisName] = !isAxisUp;
+        return isAxisUp;
     }
 }
