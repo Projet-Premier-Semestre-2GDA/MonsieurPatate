26d7ed196a3f15eaafa0c2712564c40e775c83e6
Arcanis <bastienbarth@gmail.com>
2020-01-22 18:00:22 +0100

    Revert "Plusieurs modifications (FMOD, modèles, matériaux)"
    This reverts commit 55451bb5def12026256327a21dfc01d972d346e7.

--- Monsieur Patate V2/Assets/Scripts/OoskaCustom.cs
+++ Monsieur Patate V2/Assets/Scripts/OoskaCustom.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public static class OoskaCustom {
     static Dictionary<string, bool> AxisDownDict = new Dictionary<string, bool>();
+
     public static bool GetAxisDown(string axisName) {
         float absoluteAxisValue = Mathf.Abs(Input.GetAxisRaw(axisName));
         bool isAxisDown = absoluteAxisValue > 0.1;
@@ -20,22 +21,5 @@ public static class OoskaCustom {
         
         AxisDownDict[axisName] = isAxisDown;
         return isAxisDown;
-    }
-
-    public static bool GetAxisUp(string axisName) {
-        float absoluteAxisValue = Mathf.Abs(Input.GetAxisRaw(axisName));
-        bool isAxisUp = absoluteAxisValue < 0.1;
-        
-        //Ajouter l'axe au dictionaire
-        if (!AxisDownDict.ContainsKey(axisName)) {
-            AxisDownDict.Add(axisName, true);
-        }
-
-        if (isAxisUp && !AxisDownDict[axisName]) {
-            return false;
-        }
-
-        AxisDownDict[axisName] = !isAxisUp;
-        return isAxisUp;
     }
 }
