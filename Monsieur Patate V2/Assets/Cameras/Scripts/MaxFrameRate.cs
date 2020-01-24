c260c40311427ad85bfda7748daac082a5531959
Iannis Poquet <ianpo@live.fr>
2020-01-24 04:23:08 +0100

    Changement post build 2
    Après quelque test, la source de l'augmentation de la caméra a été trouver, il s'agit d'un problème de delta time.
    
    Pour des raisons qui ne s'explique pas, si l'on rajoute du delta time, la caméra fais n'importe quoi, par consequent nous n'en mettons pas, mais pour palier a ce problème, j'ai décider de mettre un max framerate a 60 fps, ainsi, on ne pourra pas aller trop haut et donc augmenter trop la vitesse de la caméra !
    
    J'ai également tweak les valeurs de speed de la caméra pour être à bonne vitesse.

---
+++ Monsieur Patate V2/Assets/Cameras/Scripts/MaxFrameRate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxFrameRate : MonoBehaviour
+{
+    public int maxFrameRate = 60;
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;  // VSync must be disabled
+        Application.targetFrameRate = maxFrameRate;
+    }
+}
