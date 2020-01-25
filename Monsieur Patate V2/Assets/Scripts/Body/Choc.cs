10c2c810ee84ffd20d3b0874cd22db1ccbd7c003
Iannis Poquet <ianpo@live.fr>
2020-01-25 17:08:47 +0100

    Quelque modifs
    choix combien de temps avant recommencer un son (avec un peu de random)

--- Monsieur Patate V2/Assets/Scripts/Body/Choc.cs
+++ Monsieur Patate V2/Assets/Scripts/Body/Choc.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class Choc : MonoBehaviour
 {
+    public float waitTime = 0f;
+    public float randomAdd = 0;
+    private bool canPlay;
+    private float nextTime = 0;
     private void OnCollisionEnter(Collision collision)
     {
-        FMODUnity.RuntimeManager.PlayOneShot("event:/Collision");
+        if (canPlay)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Collision");
+            canPlay = false;
+            nextTime = Time.time + waitTime + Random.Range(-randomAdd,randomAdd);
+        }
+    }
+
+    private void Update()
+    {
+        if (Time.time >= nextTime && !canPlay)
+        {
+            canPlay = true;
+        }
     }
 }
